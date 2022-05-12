5b452aaff707d83d12c0322d5540aa5ca936b222
Alexander Alvarenga <moltron_nano@hotmail.com>
2022-05-11 21:47:38 -0600

    feat: initial microservice setup

---
+++ src/test/java/com/tudux/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tudux.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
