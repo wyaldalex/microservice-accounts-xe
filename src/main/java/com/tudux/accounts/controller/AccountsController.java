552d45098f17acdadd2b8a5f5ffc7177deee59a3
Alexander Alvarenga <moltron_nano@hotmail.com>
2022-05-19 21:09:32 -0600

    feat: add consumer config setup

--- src/main/java/com/tudux/accounts/controller/AccountsController.java
+++ src/main/java/com/tudux/accounts/controller/AccountsController.java
@@ -1,12 +1,18 @@
 package com.tudux.accounts.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.tudux.accounts.config.AccountsServiceConfig;
 import com.tudux.accounts.model.Accounts;
 import com.tudux.accounts.model.Customer;
+import com.tudux.accounts.model.Properties;
 import com.tudux.accounts.repository.AccountsRepository;
 
 /**
@@ -19,6 +25,9 @@ public class AccountsController {
 	
 	@Autowired
 	private AccountsRepository accountsRepository;
+	
+	@Autowired
+	AccountsServiceConfig accountsConfig;
 
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -30,6 +39,15 @@ public class AccountsController {
 			return null;
 		}
 
+	}
+	
+	@GetMapping("/account/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(accountsConfig.getMsg(), accountsConfig.getBuildVersion(),
+				accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
 	}
 
 }
