9b809e1b2fe8ac91191affffa399fc4346b37551
Alexander Alvarenga <moltron_nano@hotmail.com>
2022-05-25 21:33:38 -0600

    feat: add feign client setup to consumer endpoints from other microservices

--- src/main/java/com/tudux/accounts/controller/AccountsController.java
+++ src/main/java/com/tudux/accounts/controller/AccountsController.java
@@ -1,5 +1,7 @@
 package com.tudux.accounts.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,9 +13,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.tudux.accounts.config.AccountsServiceConfig;
 import com.tudux.accounts.model.Accounts;
+import com.tudux.accounts.model.Cards;
 import com.tudux.accounts.model.Customer;
+import com.tudux.accounts.model.CustomerDetails;
+import com.tudux.accounts.model.Loans;
 import com.tudux.accounts.model.Properties;
 import com.tudux.accounts.repository.AccountsRepository;
+import com.tudux.accounts.service.client.CardsFeignClient;
+import com.tudux.accounts.service.client.LoansFeignClient;
 
 /**
  * @author Eazy Bytes
@@ -28,6 +35,12 @@ public class AccountsController {
 	
 	@Autowired
 	AccountsServiceConfig accountsConfig;
+	
+	@Autowired
+	LoansFeignClient loansFeignClient;
+
+	@Autowired
+	CardsFeignClient cardsFeignClient;	
 
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -49,5 +62,20 @@ public class AccountsController {
 		String jsonStr = ow.writeValueAsString(properties);
 		return jsonStr;
 	}
+	
+	@PostMapping("/myCustomerDetails")
+	public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
+		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+		List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+		List<Cards> cards = cardsFeignClient.getCardDetails(customer);
+
+		CustomerDetails customerDetails = new CustomerDetails();
+		customerDetails.setAccounts(accounts);
+		customerDetails.setLoans(loans);
+		customerDetails.setCards(cards);
+		
+		return customerDetails;
+
+	}	
 
 }
